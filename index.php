bdddfcc1b1f229de9bb3c113c53d97877d090b03
wudaogang <assassin0905@gmail.com>
2013-08-08 19:03:40 +0800

    ssssssss

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
 <?php
 
-echo phpinfo();
-
-echo "xixi";
-
-echo 22233;
-
-echo 22233;
-echo 22233;
-
-echo 22233;
-echo 22233;
-echo 22233;
-
-
-
-echo 'new get time';
-echo 'news edit webwewewefafwe';
-echo 11;
-echo 11;echo 11;echo 11;echo 11;
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-echo 'newsssssss';
-
-echo "xixixixi";
-echo "xixixixi";
-echo "xixixixi";
-echo "xixixixi";
 echo "xixixixi";
 
 ?>
